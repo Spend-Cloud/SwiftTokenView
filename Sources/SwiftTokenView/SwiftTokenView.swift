fc86cc60279f891bddac8f54bbde3317f7d14376
Reshad Farid <reshad.farid@visma.com>
2021-12-16 12:08:43 +0100

    Added support for swiftui on StaticTokenView
    Added support for swiftui on StaticTokenView
    
    Added support for swiftui on StaticTokenView
    
    Added support for swiftui on StaticTokenView
    
    Added support for swiftui on StaticTokenView
    
    Added support for swiftui on StaticTokenView
    
    Added support for swiftui on StaticTokenView
    
    Added support for swiftui on StaticTokenView
    
    Added support for swiftui on StaticTokenView
    
    Added support for color
    
    Added support for color
    
    Added support for color
    
    Added support for color
    
    Added support for color
    
    Added support for color
    
    Added support for color
    
    Added support for color
    
    Added support for color

--- Sources/SwiftTokenView/SwiftTokenView.swift
+++ Sources/SwiftTokenView/SwiftTokenView.swift
@@ -1,6 +1,29 @@
-public struct SwiftTokenView {
-    public private(set) var text = "Hello, World!"
+//
+//  StaticTokenView.swift
+//
+//
+//  Created by Reshad Farid on 16/12/2021.
+//
 
-    public init() {
+import SwiftUI
+
+public struct SwiftTokenView: UIViewRepresentable {
+    
+    public let style: TokenStyle
+    @Binding public var tokens: [StaticToken]
+    
+    public init(tokenStyle: TokenStyle? = nil, tokens: Binding<[StaticToken]>) {
+        self.style = tokenStyle ?? DefaultStyle()
+        self._tokens = tokens
+    }
+    
+    public func makeUIView(context: Context) -> some StaticTokenView {
+        let view = StaticTokenView()
+        view.style = style
+        return view
+    }
+    
+    public func updateUIView(_ uiView: UIViewType, context: Context) {
+        (uiView as StaticTokenView).addTokens(tokens)
     }
 }
